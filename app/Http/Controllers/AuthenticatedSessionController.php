5ac3d9c0fab348be639306dd28e0c20da08d2eab
Muhammad Fikri <udinsaprudin1456@gmail.com>
2023-10-13 06:53:41 +0700

    Adding some syntax in app/Http/Controller/AuthenticatedSessionController.php

--- app/Http/Controllers/AuthenticatedSessionController.php
+++ app/Http/Controllers/AuthenticatedSessionController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class AuthenticatedSessionController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    protected function redirectTo()
+    {
+        return '/dashboard';
+    }
 }
