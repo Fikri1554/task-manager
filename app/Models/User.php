243956dbea8172dd27f766e91eef801914618857
Muhammad Fikri <udinsaprudin1456@gmail.com>
2023-10-13 06:46:19 +0700

    Adding TwoFactorAuthenticatable objects in app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use TwoFactorAuthenticatable, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
