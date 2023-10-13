9b226fb4c863caa0f936d58e112c3cd1fba01f83
Muhammad Fikri <udinsaprudin1456@gmail.com>
2023-10-13 10:02:20 +0700

    Deleted use Laravel\Fortify\HandlesAuthenticationResponses; in app/Http/Controllers/AuthenticatedSessionController.php

--- app/Http/Controllers/AuthenticatedSessionController.php
+++ app/Http/Controllers/AuthenticatedSessionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Laravel\Fortify\LoginViewResponse;
-use Laravel\Fortify\HandlesAuthenticationResponses;
 
 
 class AuthenticatedSessionController extends Controller
