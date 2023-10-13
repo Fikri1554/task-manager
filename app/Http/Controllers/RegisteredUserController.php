fb5fd229ebf789a27e2c696f267ec6d1c7de6f0d
Muhammad Fikri <udinsaprudin1456@gmail.com>
2023-10-13 07:06:17 +0700

    Adding some functionality for RegisteredUserController

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class RegisteredUserController extends Controller
 {
-    //
+    use RegistersUsers;
+
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    protected function validator(array $data)
+    {
+        $validator = Validator::make($data, [
+            'name' => ['required','string','max:255'],
+            'email' => ['required','string', 'email','max:255', 'unique:users'],
+            'password' => ['required','string','min:8', 'confirmed'],
+        ]);
+
+        if ($validator->fails()){            
+            return redirect('/register')
+                ->withErrors($validator)->withInput();
+        }
+
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
 }
